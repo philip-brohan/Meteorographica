194cfa77c7dfc612c9c8b69e4d8149e82e761ce2
Philip Brohan <philip@brohan.org>
2018-01-16 14:56:23 +0000

    Added a couple of tests for .data.cera20c.

---
+++ data/cera20c/test.py
@@ -0,0 +1,61 @@
+# (C) British Crown Copyright 2017, Met Office
+#
+# This code is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+"""
+Test cases for Meteorographica.data.cera20c.
+
+"""
+
+import os
+import unittest
+from Meteorographica.data import cera20c
+ 
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test_translate_for_variable_names(self):
+        self.assertEqual(cera20c.translate_for_variable_names('prmsl'),
+                         'mslp')
+        self.assertEqual(cera20c.translate_for_variable_names('prate'),
+                         'tp')
+        self.assertEqual(cera20c.translate_for_variable_names('uwnd.10m'),
+                         'u10')
+        with self.assertRaises(StandardError) as cm:
+            cera20c.translate_for_variable_names('mslp')
+        self.assertIn('Unsupported variable mslp',
+                      cm.exception)
+
+    def test_translate_for_file_names(self):
+        self.assertEqual(cera20c.translate_for_file_names('prmsl'),
+                         'mslp')
+        self.assertEqual(cera20c.translate_for_file_names('prate'),
+                         'tp')
+        self.assertEqual(cera20c.translate_for_file_names('uwnd.10m'),
+                         '10u')
+        with self.assertRaises(StandardError) as cm:
+            cera20c.translate_for_file_names('mslp')
+        self.assertIn('Unsupported variable mslp',
+                      cm.exception)
+
+    def test_get_data_dir(self):
+        scratch=os.getenv('SCRATCH')
+        del os.environ['SCRATCH']
+        with self.assertRaises(StandardError) as cm:
+            cera20c.get_data_dir()
+        self.assertIn('SCRATCH environment variable is undefined',
+                      cm.exception)
+        os.environ['SCRATCH']=scratch
+        
+if __name__ == '__main__':
+    unittest.main()
