c7aa489bd500f36ca75e618179072d2be59ac7af
Philip Brohan <philip@brohan.org>
2018-01-17 09:44:37 +0000

    Add two files I forgot to commit to .data.cera20c.

---
+++ data/cera20c/__init__.py
@@ -0,0 +1,21 @@
+# (C) British Crown Copyright 2017, Met Office
+#
+# This code is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+"""
+A package for handling data from the CERA-20C reanalysis.
+
+"""
+
+from utils import *
+from fetch import *
+from load import *
+
