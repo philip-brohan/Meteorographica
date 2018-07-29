0cf4355f48a179ba0e637554465598303a6d3506
Philip Brohan <philip@brohan.org>
2018-07-29 15:12:25 +0100

    Renamed .pages dir to avoid conflict with pages app.

--- setup.py
+++ setup.py
@@ -50,7 +50,9 @@ setup(
 
     # Other packages that your project depends on.
     # iris>2 - does not work - can't find any iris?
+    #    'cartopy>0.15',
     install_requires=[
+        'iris>2.0',
         'cartopy>0.15',
         'numpy>1.13',
         'scipy>0.18',
